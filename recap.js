5fbdba186b8ead8fa9c8e65c5e5b2bcac818da54
Joaneeleng <joaneeleng@gmail.com>
2022-09-06 22:22:00 +0800

    first commit

---
+++ recap.js
@@ -0,0 +1,8 @@
+let first_name = "Jon";
+let last_name = "Scheele";
+
+// let full_name = first_name + last_name;
+// // full_name = "DixantMittal"
+
+console.log(`Hello there! ${full_name}`);
+// console.log("Hello there! ${full_name}");
